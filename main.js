72b24f041f51eb1cb1cdae0bbbadd4d890b5c5f0
eoghks7316 <eoghks8600@gmail.com>
2022-08-09 20:00:24 +0900

    Add Middleware
    Add compression, bodyParser

--- main.js
+++ main.js
@@ -8,51 +8,55 @@ var bodyParser = require('body-parser');
 const app = express()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+/***** 미들웨어 작성  *****/
+app.get('*', function (req, res, next) {
+  fs.readdir('./data', function (error, filelist) {
+    req.list = filelist;
+    next();
+  });
+})
 //route = path마다 정당한 응답
 
 /*********** Read ************/
-app.get('/', (req, res) =>
-  fs.readdir('./data', function (error, filelist) {
-    var title = 'Welcome';
-    var description = 'Hello, Node.js';
-    var list = template.list(filelist);
-    var html = template.HTML(title, list,
-      `<h2>${title}</h2>${description}`,
-      `<a href="/create">create</a>`
-    );
-    res.send(html);
-  }))
+app.get('/', function (req, res) {
+  var title = 'Welcome';
+  var description = 'Hello, Node.js';
+  var list = template.list(req.list);
+  var html = template.HTML(title, list,
+    `<h2>${title}</h2>${description}`,
+    `<a href="/create">create</a>`
+  );
+  res.send(html);
+})
 
 app.get('/page/:pageId', function (req, res) {
-  fs.readdir('./data', function (error, filelist) {
-    var filteredId = path.parse(req.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-      var title = req.params.pageId
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ['h1']
-      });
-      var list = template.list(filelist);
-      var html = template.HTML(sanitizedTitle, list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/create">create</a>
+  var filteredId = path.parse(req.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
+    var title = req.params.pageId
+    var sanitizedTitle = sanitizeHtml(title);
+    var sanitizedDescription = sanitizeHtml(description, {
+      allowedTags: ['h1']
+    });
+    var list = template.list(req.list);
+    var html = template.HTML(sanitizedTitle, list,
+      `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+      ` <a href="/create">create</a>
           <a href="/update/${sanitizedTitle}">update</a>
           <form action="/delete" method="post">
             <input type="hidden" name="id" value="${sanitizedTitle}">
             <input type="submit" value="delete">
           </form>`
-      );
-      res.send(html);
-    });
+    );
+    res.send(html);
   });
 })
 
-/*********** create ************/
+/*********** Create ************/
 app.get('/create', function (req, res) {
-  fs.readdir('./data', function (error, filelist) {
-    var title = 'WEB - Create';
-    var list = template.list(filelist);
-    var html = template.HTML(title, list, `
+  var title = 'WEB - Create';
+  var list = template.list(req.list);
+  var html = template.HTML(title, list, `
       <form action="/create" method="post">
         <p><input type="text" name="title" placeholder="title"></p>
         <p>
@@ -63,8 +67,7 @@ app.get('/create', function (req, res) {
         </p>
       </form>
     `, '');
-    res.send(html);
-  });
+  res.send(html);
 })
 
 app.post('/create', function (req, res) {
@@ -73,15 +76,14 @@ app.post('/create', function (req, res) {
   })
 })
 
-/*********** update ************/
+/*********** Update ************/
 app.get('/update/:pageId', function (req, res) {
-  fs.readdir('./data', function (error, filelist) {
-    var filteredId = path.parse(req.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-      var title = req.params.pageId;
-      var list = template.list(filelist);
-      var html = template.HTML(title, list,
-        `
+  var filteredId = path.parse(req.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
+    var title = req.params.pageId;
+    var list = template.list(req.list);
+    var html = template.HTML(title, list,
+      `
          <form action="/update" method="post">
            <input type="hidden" name="id" value="${title}">
            <p><input type="text" name="title" placeholder="title" value="${title}"></p>
@@ -93,10 +95,9 @@ app.get('/update/:pageId', function (req, res) {
            </p>
          </form>
          `,
-        `<a href="/create">create</a> <a href="/update/${title}">update</a>`
-      );
-      res.send(html)
-    });
+      `<a href="/create">create</a> <a href="/update/${title}">update</a>`
+    );
+    res.send(html)
   });
 })
 
@@ -108,7 +109,7 @@ app.post('/update', function (req, res) {
   });
 })
 
-/*********** delete ************/
+/*********** Delete ************/
 app.post('/delete', function (req, res) {
   fs.unlink(`data/${req.body.id}`, function (error) {
     res.redirect('/')
@@ -117,7 +118,7 @@ app.post('/delete', function (req, res) {
 })
 
 /*********** listen ************/
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT | 3000, () => console.log('Example app listening on port 3000!'))
 
 /*
 var http = require('http');
@@ -148,19 +149,7 @@ var app = http.createServer(function(request,response){
     } else if(pathname === '/update_process'){
       
     } else if(pathname === '/delete_process'){
-      var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-          var post = qs.parse(body);
-          var id = post.id;
-          var filteredId = path.parse(id).base;
-          fs.unlink(`data/${filteredId}`, function(error){
-            response.writeHead(302, {Location: `/`});
-            response.end();
-          })
-      });
+      
     } else {
       response.writeHead(404);
       response.end('Not found');
